033da2a040501a861de0d26bc4588c3fc4294f20
MaevaASSOGHO <tabouretgangbusiness@gmail.com>
2026-04-25 12:46:07 +0000

    bookings

--- savage-club/app/api/bookings/[id]/route.ts
+++ savage-club/app/api/bookings/[id]/route.ts
@@ -3,6 +3,7 @@ import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { NextResponse } from "next/server";
+import crypto from "crypto";
 
 const MAX_NEGOTIATIONS = 2;
 
@@ -15,7 +16,13 @@ async function createBookingNotif(
   bookingId: string
 ) {
   await tx.notification.create({
-    data: { type, receiverId, senderId, bookingId } as any,
+    data: {
+      id: crypto.randomUUID(), // ← ajouter cette ligne
+      type,
+      receiverId,
+      senderId,
+      bookingId,
+    } as any,
   });
 }
 
