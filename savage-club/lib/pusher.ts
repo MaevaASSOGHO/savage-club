af9446d59aa3a2f5286686d3394a04a890820cfb
MaevaASSOGHO <tabouretgangbusiness@gmail.com>
2026-05-15 15:22:32 +0000

    pusher

--- savage-club/lib/pusher.ts
+++ savage-club/lib/pusher.ts
@@ -1,7 +1,11 @@
 // lib/pusher.ts
 export async function getPusher() {
-  const Pusher = (await import("pusher")).default;
-  return new Pusher({
+  // require() au lieu de import() pour éviter le problème CJS/ESM avec Turbopack
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const PusherModule = require("pusher");
+  const PusherClass = PusherModule.default || PusherModule;
+  
+  return new PusherClass({
     appId:   process.env.PUSHER_APP_ID!,
     key:     process.env.PUSHER_KEY!,
     secret:  process.env.PUSHER_SECRET!,
